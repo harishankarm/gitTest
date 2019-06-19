27ee6c6517cd7e0272cb7d485da43d3d46964c30
hmunusamy <hmunusamy@ebay.com>
2019-06-19 16:05:42 +0530

    one

--- src/main/java/com/git/test/Samp.java
+++ src/main/java/com/git/test/Samp.java
@@ -1,14 +1,10 @@
 package com.git.test;
 
-public class Test {
+public class Samp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		System.out.println("Test");
 
-		
-		System.out.println("Test");
 	}
 
 }
